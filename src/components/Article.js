ebc16622cae0772ecea7b910a3fa683ca777d36f
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-26 12:25:34 +0300

    Completed bonus assignment.

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,10 +6,29 @@ function Article({
     preview,
     minutes })
     { 
+        function handleMinutes(minutes) {
+            const coffeeEmoji = '☕️', bentoEmoji = '🍱'
+
+            if(minutes < 30) {
+                if(minutes <= 5) {
+                    return <> {coffeeEmoji} {minutes} minutes read </>
+                } else if ( minutes > 5 && minutes <= 10) {
+                    return  <>{coffeeEmoji}{coffeeEmoji} {minutes} minutes read</>
+                    
+                } else if ( minutes > 10 && minutes <= 15) {
+                    return <>{coffeeEmoji}{coffeeEmoji}{coffeeEmoji} {minutes} minutes read</>
+                }
+            } else if (minutes >= 30) {
+                if(minutes > 40 && minutes <= 50) {
+                    return <>{bentoEmoji}{bentoEmoji}{bentoEmoji}{bentoEmoji}{bentoEmoji} {minutes} minutes read</>
+                }
+            }
+        }
+
         return (
         <article>
             <h3>{title}</h3>
-            <small>{date} {minutes} minutes to read </small>
+            <small>{date}. &nbsp; {handleMinutes(minutes)} </small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,7 +3,7 @@ import Article from "./Article";
 
 function ArticleList({blogPosts}) {
 
-    const articleItem = blogPosts.map(post => <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes = {post.minutes}/>)
+    const articleItem = blogPosts.map(post => <Article key = {post.id} title = {post.title} date = {post.date} preview = {post.preview} minutes = {post.minutes}/>)
 
     return (
         <main>
